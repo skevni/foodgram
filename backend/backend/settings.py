b971d7258d034d0b187d5607548c4f638755fefa
Sklyarov Evgenii <32865883+skevni@users.noreply.github.com>
2026-02-03 16:23:23 +0300

    Docker compose (#17)
    * db port
    
    * fix
    
    * fix
    
    * +
    
    ---------
    
    Co-authored-by: E.N.Sklyarov <e.sklyarov@tns-e.ru>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,17 +68,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB', 'django'),
-            'USER': os.getenv('POSTGRES_USER', 'django'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-                'USER': os.getenv('POSTGRES_USER', 'postgres_user'),
-                'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres_password'),
-                'HOST': os.getenv('DB_HOST', 'db'),
-                'PORT': os.getenv('DB_PORT', '5432'),
-            }
+            'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres_user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres_password'),
+            'HOST': os.getenv('DB_HOST', 'db'),
+            'PORT': os.getenv('DB_PORT', '5432'),
         }
     }
 
@@ -129,10 +123,8 @@ REST_FRAMEWORK = {
 
 }
 
-CORS_ORIGIN_WHITELIST = os.getenv(
-    'CORS_ORIGIN_WHITELIST', 'http://localhost:80, http://127.0.0.1:80').split(', ')
 CSRF_TRUSTED_ORIGINS = os.getenv(
-    'CORS_ORIGIN_WHITELIST', 'http://localhost').split(', ')
+    'CORS_ORIGIN_WHITELIST', 'http://localhost,http://127.0.0.1').split(', ')
 
 AUTH_USER_MODEL = 'cookbook.User'
 
